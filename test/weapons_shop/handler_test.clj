94b84b7dc6025a0b3db79289ef9585eb188ea29f
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-09-28 16:04:12 -0700

    Begin to test the new API

--- test/weapons_shop/handler_test.clj
+++ test/weapons_shop/handler_test.clj
@@ -15,3 +15,28 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest test-weapon-api
+  (testing "GET all empty"
+    (let [response (app (mock/request :get "/weapon"))
+          body (json/parse-string (:body response) true)]
+      (is (= (:status response) 200))
+      (is (= (:weapons body) []))))
+
+  (def weapon-id (atom nil))
+
+  (testing "POST a new weapon"
+    (let [response (app (-> (mock/request :post "/weapon")
+                             (mock/json-body {:name "Test Sword" :damage 5 :cost 100})))
+          body (json/parse-string (:body response) true)]
+      (is (= (:status response) 200))
+      (is (= (:name body) "Test Sword"))
+      (is (:id body))
+      (reset! weapon-id (:id body))))
+
+  (testing "GET that weapon back"
+    (let [response (app (mock/request :get (str "/weapon/" @weapon-id)))
+          body (json/parse-string (:body response) true)]
+      (is (= (:status response) 200))
+      (is (= (:id body) @weapon-id))
+      (is (= (:name body) "Test Sword")))))
