0b5718846094563473ad5db992a2abbac5db16ad
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-09-29 15:45:28 -0700

    Remove the fake DB

--- src/weapons_shop/db.clj
+++ src/weapons_shop/db.clj
@@ -9,9 +9,6 @@
 ;; HACK
 (create-weapon-table db-config-dev)
 
-(def fake-db (atom {}))
-(def next-id (atom 0))
-
 (defn get-all-weapons []
   (get-all-weapons-query db-config-dev))
 
@@ -19,14 +16,17 @@
   (get-weapon-query db-config-dev {:id (Integer/parseInt id)}))
 
 (defn new-weapon [weapon]
-  (let [id (swap! next-id inc)
-        weapon-with-id (assoc weapon :id id)
-        db-weapon (insert-weapon-query db-config-dev (assoc weapon :price (:cost weapon) :durability (:durability weapon)))]
-    (swap! fake-db assoc (str id) weapon-with-id)
-    db-weapon))
+  (insert-weapon-query db-config-dev
+                       (assoc weapon
+                              :price (:cost weapon)
+                              :durability (:durability weapon))))
 
 (defn update-weapon [id weapon]
-  (update-weapon-query db-config-dev (assoc weapon :price (:cost weapon) :durability (:durability weapon) :id (Integer/parseInt id))))
+  (update-weapon-query db-config-dev
+                       (assoc weapon
+                              :price (:cost weapon)
+                              :durability (:durability weapon)
+                              :id (Integer/parseInt id))))
 
 (defn delete-weapon [id]
   (let [count (delete-weapon-query db-config-dev {:id (Integer/parseInt id)})]
